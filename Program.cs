44cf0b257d1c1e5f105aeec8617db47e2d017e81
Charlotte <zlgakingdom7@gmail.com>
2026-04-27 16:25:55 +0800

    註冊連線測試

--- Program.cs
+++ Program.cs
@@ -9,6 +9,19 @@ namespace Moody_backend
 
             // Add services to the container.
 
+            // 新增 CORS 政策 -> 允許 React 存取 API
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowReact", policy =>
+                {
+                    // 這裡填寫我 React 啟動時的網址 -> 通常 Vite 是 5173，請依我實際的 port 為準。
+                    policy.WithOrigins("http://localhost:5173")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
+
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
@@ -22,6 +35,9 @@ namespace Moody_backend
             }
 
             app.UseHttpsRedirection();
+
+            // 啟用 CORS 政策 -> 這行要放在 UseAuthorization 前面
+            app.UseCors("AllowReact");
 
             app.UseAuthorization();
 
